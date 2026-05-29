47f2d8ad181b4450d6a5e49a9b54a018ba935a00
Kevin McCarthy <kevin.e.mccarthy1983@gmail.com>
2026-05-29 07:54:24 +0100

    no time

--- backend/src/main/java/com/cephadex/ambi/session/LiveSessionOrchestrator.java
+++ backend/src/main/java/com/cephadex/ambi/session/LiveSessionOrchestrator.java
@@ -1,15 +1,35 @@
 package com.cephadex.ambi.session;
 
+import com.cephadex.ambi.session.SessionTypes.SessionId;
+import com.cephadex.ambi.session.SessionTypes.SlideId;
+import com.cephadex.ambi.session.liveSession.LiveSessionRepository;
+
 public class LiveSessionOrchestrator {
-    private final LiveSessionDomain liveSession;
+    private final LiveSessionRepository repo;
 
     // private final SessionLocks locks;
     // private final TallyStore tallies;
     // private final DeadlineScheduler deadlines;
     // private final EventPublisher publisher;
     // private final Clock clock;
-    public LiveSessionOrchestrator(LiveSessionDomain liveSessionDomain) {
+    public LiveSessionOrchestrator(LiveSessionRepository repo) {
 
-        this.liveSession = liveSessionDomain;
+        this.repo = repo;
+    }
+
+    public void startLiveSession(SessionId sid) {
+
+    }
+
+    public void startRound(SessionId sid, SlideId slideId) {
+
+    }
+
+    public void endRound(SessionId sid, SlideId slideId) {
+
+    }
+
+    public void restartRound(SessionId sid, SlideId slideId) {
+
     }
 }
